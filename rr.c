b79395bc8247e24eab851e45368998335e80aa8a
SUJAY JAYADEEP <sujayjayadeep35@gmail.com>
2026-03-05 16:02:56 +0000

    Added Round Robin scheduling program (rr.c)

--- rr.c
+++ rr.c
@@ -1,61 +1,65 @@
 #include <stdio.h>
 
 int main() {
-    int n, q;
+    int n,q;
 
-    scanf("%d", &n);
+    scanf("%d",&n);
 
     char pid[n][10];
-    int at[n], bt[n], rt[n];
-    int ct[n], tat[n], wt[n];
+    int at[n],bt[n],rt[n],ct[n],tat[n],wt[n];
 
-    for(int i = 0; i < n; i++) {
-        scanf("%s %d %d", pid[i], &at[i], &bt[i]);
-        rt[i] = bt[i];
+    for(int i=0;i<n;i++){
+        scanf("%s %d %d",pid[i],&at[i],&bt[i]);
+        rt[i]=bt[i];
     }
 
-    scanf("%d", &q);
+    scanf("%d",&q);
 
-    int time = 0, completed = 0;
+    int time=0,done;
 
-    while(completed < n) {
-        for(int i = 0; i < n; i++) {
+    while(1){
+        done=1;
 
-            if(rt[i] > 0 && at[i] <= time) {
+        for(int i=0;i<n;i++){
+            if(rt[i]>0 && at[i]<=time){
+                done=0;
 
-                if(rt[i] > q) {
-                    time += q;
-                    rt[i] -= q;
+                if(rt[i]>q){
+                    time+=q;
+                    rt[i]-=q;
                 }
-                else {
-                    time += rt[i];
-                    rt[i] = 0;
-                    ct[i] = time;
-                    completed++;
+                else{
+                    time+=rt[i];
+                    rt[i]=0;
+                    ct[i]=time;
                 }
             }
         }
+
+        if(done)
+        break;
     }
 
-    float avgwt = 0, avgtat = 0;
+    float avgwt=0,avgtat=0;
 
-    for(int i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-        avgwt += wt[i];
-        avgtat += tat[i];
+    for(int i=0;i<n;i++){
+        tat[i]=ct[i]-at[i];
+        wt[i]=tat[i]-bt[i];
+
+        avgwt+=wt[i];
+        avgtat+=tat[i];
     }
 
     printf("Waiting Time:\n");
-    for(int i = 0; i < n; i++)
-        printf("%s %d\n", pid[i], wt[i]);
+    for(int i=0;i<n;i++)
+        printf("%s %d\n",pid[i],wt[i]);
 
     printf("Turnaround Time:\n");
-    for(int i = 0; i < n; i++)
-        printf("%s %d\n", pid[i], tat[i]);
+    for(int i=0;i<n;i++)
+        printf("%s %d\n",pid[i],tat[i]);
 
-    printf("Average Waiting Time: %.2f\n", avgwt/n);
-    printf("Average Turnaround Time: %.2f\n", avgtat/n);
+    printf("Average Waiting Time: %.2f\n",avgwt/n);
+    printf("Average Turnaround Time: %.2f\n",avgtat/n);
 
     return 0;
 }
